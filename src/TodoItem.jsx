b6bdddce4e5309e7adfab2498d322f4fe74a11d4
Nina Svärd <ss225bw@student.lnu.se>
2023-06-30 13:23:42 +0200

    first commit

---
+++ src/TodoItem.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/prop-types */
+const TodoItem = ({ completed, id, title, toggleTodo, deleteTodo }) => {
+  return (
+    <li>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={completed}
+                  onChange={(e) => toggleTodo(id, e.target.checked)}
+                />
+                {title}
+              </label>
+              <button
+                className="btn btn-danger"
+                onClick={() => deleteTodo(id)}
+              >
+                Delete
+              </button>
+            </li>
+  )
+}
+
+export default TodoItem
---
+++ src/TodoList.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/prop-types */
+
+import TodoItem from "./TodoItem";
+
+const TodoList = ({ todos, toggleTodo, deleteTodo }) => {
+  return (
+    <ul className="list">
+        {todos.length === 0 ? "No Todos" : null}
+        {todos.map((todo) => {
+          return (
+            <TodoItem {...todo} key={todo.id} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
+          );
+        })}
+      </ul>
+  )
+}
+
+export default TodoList
